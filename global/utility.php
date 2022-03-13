081dc0356f8f11fcb5cc60c3761092c7289c8092
Adnan <muh.adnansuryaazis@gmail.com>
2022-03-13 10:12:29 +0800

    tglWaktuIndo, statCairan

--- global/utility.php
+++ global/utility.php
@@ -1,6 +1,51 @@
 <?php
-function generateRandomString($length = 10) {
-    return substr(str_shuffle(str_repeat($x='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil($length/strlen($x)) )),1,$length);
+
+function tglWaktuIndo($tglWaktu){
+    $pisahSpasi =  explode(' ', $tglWaktu);
+    $tanggal = $pisahSpasi[0];
+    $waktu = $pisahSpasi[1];
+
+	$bulan = array (
+		1 =>   'Januari',
+		'Februari',
+		'Maret',
+		'April',
+		'Mei',
+		'Juni',
+		'Juli',
+		'Agustus',
+		'September',
+		'Oktober',
+		'November',
+		'Desember'
+	);
+	$pecahkan = explode('-', $tanggal);
+	
+	// variabel pecahkan 0 = tanggal
+	// variabel pecahkan 1 = bulan
+	// variabel pecahkan 2 = tahun
+	if($tanggal != ''){
+		return $pecahkan[2] . ' ' . $bulan[ (int)$pecahkan[1] ] . ' ' . $pecahkan[0] . ' ('.$waktu.')';
+	}else{
+		return 'Belum Tersedia';
+	}
+	
+}
+
+function statCairan($cairan, $cairMax, $cairMin){
+
+    $batasPenuh = $cairMax * 0.9;
+    $status = 'Error';
+
+    if($cairan < $cairMin){
+        $status = 'Kosong';
+    }elseif($cairan >= $cairMin && $cairan <= $batasPenuh){
+        $status = 'Normal';
+    }elseif($cairan > $batasPenuh){
+        $status = 'Penuh';
+    }
+
+    return $status;
 }
 
 ?>
