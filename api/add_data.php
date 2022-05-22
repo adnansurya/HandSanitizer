53f0395ac11d174907e0563b1180dbc712b46c62
Adnan <muh.adnansuryaazis@gmail.com>
2022-05-23 00:02:56 +0800

    tambah keterangan

--- api/add_data.php
+++ api/add_data.php
@@ -1,14 +1,15 @@
 <?php
 
-include '../global/db_access.php';
+include '../global/db_access.php'; //mengambil file konfigurasi akses database
 
-if(isset($_GET['suhu']) && isset($_GET['cairan'])){
+if(isset($_GET['suhu']) && isset($_GET['cairan'])){ //memeriksa jika nilai suhu dan ADC cairan tersedia
 
-    $waktu = $date->format('Y-m-d H:i:s');
+    $waktu = $date->format('Y-m-d H:i:s'); //mengatur format penulisan waktu
 
+    //perintah sql untuk menambahkan data log
     $sql = "INSERT INTO log_action (suhu, cairan, cairan_max, cairan_min, waktu) VALUES ('".$_GET['suhu']."','".$_GET['cairan']."','".$_GET['max']."','".$_GET['min']."','".$waktu."')";
 
-        
+    //mengeksekusi perintah sql 
     if(!mysqli_query($conn, $sql)){
         echo("Error log: " . $conn -> error);
     }else{
